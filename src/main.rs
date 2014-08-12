32a7e53109bf899df079b8b8d6d8f355ab7becf9
Zeke Sonxx <zeke@zekesonxx.com>
2014-08-11 23:26:49 -0400

    Added proper cd functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 
 use std::io;
+use std::io::fs;
 use std::os;
-use std::io::process::{Command, Process};
+use std::io::process::{Command};
 use std::path::Path;
+
 
 fn main() {
   println!("ash Burning Fucking Shit shell")
@@ -11,20 +13,55 @@ fn main() {
     print!("{} $ ", cwd.display());
     let rawinput = io::stdin().read_line().ok().expect("Error Occured");
     let input = rawinput.as_slice().trim();
-    match input {
-      "~" => {
-        //temp, go home.
-        cwd = match os::homedir() {
-          Some(e) => Path::new(e),
-          None => Path::new("/")
+    if input == "" { continue } //skip blank enters
+    let opts: Vec<&str> = input.split_str(" ").collect();
+    let (cmd, args) = (opts[0], opts.slice(1, opts.len()));
+    match cmd {
+      "cd" => {
+        //set the current directory
+        //only works absolutely atm
+        if args[0] == "~" {
+          //go home
+          cwd = match os::homedir() {
+            Some(e) => Path::new(e),
+            None => Path::new("/")
+          };
+          continue;
+        }
+        let path: Option<Path> = if args[0].starts_with("~") {
+          match os::homedir() {
+            Some(e) => {
+              let dir = e.join(args[0].slice_from(2)); //[~/]
+              println!("{}", dir.display());
+              Some(dir)
+            }, //hacky but whatever
+            None => Path::new_opt(args[0])
+          }
+        } else if args[0].starts_with(".") { //relative dir
+          Some(cwd.join(args[0]))
+        } else {
+          Path::new_opt(args[0])
+        };
+        match path {
+          Some(new) => {
+            match fs::stat(&new) {
+              Ok(stat) => { 
+                match stat.kind {
+                  TypeDirectory => {cwd = new} // why is this so unhappy
+                } 
+              },
+              Err(e) => println!("No such file or directory: \"{}\"", args[0])
+            }
+          }
+          None => println!("Failed to locate path")
         }
       }
-      "" => {} //no input
       _ => {
-        let opts: Vec<&str> = input.split_str(" ").collect();
-        match Command::new(opts[0]).cwd(&cwd).args(opts.slice(1, opts.len())).output() {
+        let process = Command::new(cmd).cwd(&cwd).args(args).output();
+
+        match process {
           Ok(output) => {
-            println!("[{}] {}",output.status, String::from_utf8_lossy(output.output.as_slice()));
+            println!("[{}] {}", output.status, String::from_utf8_lossy(output.output.as_slice()));
 
           },
           Err(e) => {
